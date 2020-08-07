1f82970c0b620073c595d5cc46d86ffd2c5f55ae
abuhorainhero <abuhorain2016@gmail.com>
2020-08-07 20:03:20 +0600

    ture false add

---
+++ true-false.js
@@ -0,0 +1,10 @@
+// False : 0, "", undefine, null, 
+
+// true : "0", " ", [], {}
+let name = {};
+if (name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false.")
+}
